0174ec3448a910f576a5d8891c4bbbee0841aecf
hritik <86347286+hritikd3@users.noreply.github.com>
2022-02-27 18:04:22 +0000

    custom css added

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,20 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 function App() {
-  const [markdown,setMArkdown]=useState('## markdown')
+  const [markdown, setMarkdown] = useState('# markdown preview')
+
   return (
-    <main>   
-<section className="markdown" >
-<textarea className="input" value={markdown} onChange={(e)=> setMarkdown(e.target.value)}
-  > </textarea>
-
-  <article className="result ">  
-<ReactMarkdown>
-  {markdown}
-</ReactMarkdown>
-
-  </article>
-
-
-
-</section>
-
+    <main>
+      <section className='markdown'>
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className='result'>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
     </main>
   )
 }
