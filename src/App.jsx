0f54bf09a78e1de4638ca5f1eb25bcac9eb7103f
hritik <86347286+hritikd3@users.noreply.github.com>
2022-02-27 18:01:28 +0000

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 function App() {
+  const [markdown,setMArkdown]=useState('## markdown')
   return (
-    <main>
-     hey in
+    <main>   
+<section className="markdown" >
+<textarea className="input" value={markdown} onChange={(e)=> setMarkdown(e.target.value)}
+  > </textarea>
+
+  <article className="result ">  
+<ReactMarkdown>
+  {markdown}
+</ReactMarkdown>
+
+  </article>
+
+
+
+</section>
+
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
